fa0b1c9a5f4699a2e4da0a59db03028a1a4f6e88
foreach <112372790+Julioyo@users.noreply.github.com>
2024-02-01 11:02:12 +0100

    010220241102

--- cleanuxfront.py
+++ cleanuxfront.py
@@ -1,45 +1,22 @@
 import tkinter as tk
-from tkinter import messagebox, scrolledtext
+from tkinter import messagebox
 import subprocess
 
-def execute_bash_script(output_text):
-    print("Début de l'exécution du script Bash")
+def execute_bash_script():
     try:
-        process = subprocess.Popen(["bash", "cleanux.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=1)
+        process = subprocess.Popen(["bash", "cleanux.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
-        def read_output():
+        while True:
             output_line = process.stdout.readline()
-            if output_line:
-                print(output_line, end='')  # Pour débogage
-                output_text.insert(tk.END, output_line)
-                output_text.see(tk.END)
-                output_text.update()
+            if output_line == '' and process.poll() is not None:
+                break
 
-                # Continue de surveiller la sortie du processus
-                root.after(1, read_output)
-            elif process.poll() is None:
-                # Si le processus n'est pas encore terminé, continue de surveiller
-                root.after(100, read_output)
-            else:
-                # Le processus est terminé
-                finalize_output_text()
-
-        # Démarre la surveillance de la sortie du processus
-        root.after(1, read_output)
-
+        process.communicate()  # Attend la fin du processus
+        messagebox.showinfo("Succès", "Le script Bash a été exécuté avec succès.")
     except Exception as e:
-        print(f"Erreur lors de l'exécution du script Bash : {str(e)}")
         messagebox.showerror("Erreur", f"Erreur lors de l'exécution du script Bash : {str(e)}")
 
-def finalize_output_text():
-    output_text.insert(tk.END, "Nettoyage terminé\n")
-    output_text.see(tk.END)
-    output_text.update()
-    print("Fin de l'exécution du script Bash")
-    messagebox.showinfo("Succès", "Le script Bash a été exécuté avec succès.")
-
 def main():
-    global root  # Déclarer root en tant que variable globale
     root = tk.Tk()
     root.title("Cleanux")
 
@@ -47,7 +24,7 @@ def main():
     root.configure(bg="#f8f9fa")
 
     # Ajout d'un en-tête stylisé
-    header_label = tk.Label(root, text="Bienvenue dans Cleanux", font=("Helvetica", 18), bg="#007BFF", fg="white", padx=10, pady=10)
+    header_label = tk.Label(root, text="Welcome to Cleanux", font=("Helvetica", 18), bg="#007BFF", fg="white", padx=10, pady=10)
     header_label.pack(fill="x")
 
     # Ajout d'un espace
@@ -55,24 +32,19 @@ def main():
     space_label.pack()
 
     # Ajout d'une description
-    description_label = tk.Label(root, text="Cliquez sur le bouton ci-dessous pour exécuter le nettoyage du système.", font=("Helvetica", 12), bg="#f8f9fa", padx=10, pady=10)
+    description_label = tk.Label(root, text="Linux cleaning tool", font=("Helvetica", 12), bg="#f8f9fa", padx=10, pady=10)
     description_label.pack()
 
-    # Ajout d'un panneau pour la sortie du terminal
-    global output_text  # Déclarer output_text en tant que variable globale
-    output_text = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=50, height=10, state=tk.DISABLED)
-    output_text.pack(padx=10, pady=10)
-
     # Ajout d'un bouton
-    button = tk.Button(root, text="Nettoyer le système", command=lambda: execute_bash_script(output_text), font=("Helvetica", 14), bg="#007BFF", fg="white", padx=20, pady=10, relief="flat")
+    button = tk.Button(root, text="Clean Linux", font=("Helvetica", 14), bg="#007BFF", fg="white", padx=20, pady=10, relief="flat", command=execute_bash_script)
     button.pack()
 
     # Ajout d'un pied de page
-    footer_label = tk.Label(root, text="pour La Capsule", font=("Helvetica", 10), bg="#007BFF", fg="white", padx=10, pady=5)
+    footer_label = tk.Label(root, text="for La Capsule", font=("Helvetica", 10), bg="#007BFF", fg="white", padx=10, pady=5)
     footer_label.pack(fill="x", side="bottom")
 
     # Ajout d'un pied de page
-    footer_label = tk.Label(root, text="foreach en personne", font=("Helvetica", 10), bg="#007BFF", fg="white", padx=10, pady=5)
+    footer_label = tk.Label(root, text=" by foreach herself", font=("Helvetica", 10), bg="#007BFF", fg="white", padx=10, pady=5)
     footer_label.pack(fill="x", side="bottom")
 
     root.mainloop()
