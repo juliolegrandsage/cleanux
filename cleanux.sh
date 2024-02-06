e27b057ce7b30223084783d18ecaaebe05ab61ea
foreach <112372790+Julioyo@users.noreply.github.com>
2024-02-06 15:47:34 +0100

    Add files via upload

--- cleanux.sh
+++ cleanux.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Fonction pour afficher la barre de progression
+show_progress() {
+    # Afficher la barre de progression avec zenity
+    zenity --progress --title="Cleaning Progress" --text="Cleaning in progress..." --percentage=0 --auto-close --width=400
+}
+
 # Récupérer les arguments
 CLEAN_TEMP=$1
 CLEAN_WINDOWS_TEMP=$2
@@ -7,34 +13,58 @@ CLEAN_PREFETCH=$3
 CLEAN_UPDATE_CACHE=$4
 CLEAN_LOGS=$5
 
-# Nettoyer le dossier Temp si la case à cocher est cochée
-if [ "$CLEAN_TEMP" -eq 1 ]; then
-    echo "Cleaning Temp folder..."
-    sudo rm -rf /var/tmp/*
+# Vérifier si les variables sont définies et sont des nombres entiers
+if [[ -n "$CLEAN_TEMP" && "$CLEAN_TEMP" =~ ^[0-9]+$ ]]; then
+    # Nettoyer le dossier Temp spécifique à Linux Mint si la case à cocher est cochée
+    if [ "$CLEAN_TEMP" -eq 1 ]; then
+        echo "Cleaning Linux Mint Temp folder..."
+        sudo rm -rf /tmp/*
+    fi
+else
+    echo "Error: CLEAN_TEMP is not defined or not an integer."
 fi
 
-# Nettoyer le dossier Temp de Windows si la case à cocher est cochée
-if [ "$CLEAN_WINDOWS_TEMP" -eq 1 ]; then
-    echo "Cleaning Windows Temp folder..."
-    sudo rm -rf /tmp/*
+if [[ -n "$CLEAN_MINT_TEMP" && "$CLEAN_MINT_TEMP" =~ ^[0-9]+$ ]]; then
+    # Nettoyer le dossier Temp de Windows si la case à cocher est cochée
+    if [ "$CLEAN_MINT_TEMP" -eq 1 ]; then
+        echo "Cleaning Mint Temp folder..."
+        sudo rm -rf /var/tmp/*
+    fi
+else
+    echo "Error: CLEAN_MINT_TEMP is not defined or not an integer."
 fi
 
-# Nettoyer le dossier Prefetch si la case à cocher est cochée
-if [ "$CLEAN_PREFETCH" -eq 1 ]; then
-    echo "Cleaning Prefetch folder..."
-    sudo rm -rf /var/log/apt/*
+if [[ -n "$CLEAN_PREFETCH" && "$CLEAN_PREFETCH" =~ ^[0-9]+$ ]]; then
+    # Nettoyer le dossier Prefetch si la case à cocher est cochée
+    if [ "$CLEAN_PREFETCH" -eq 1 ]; then
+        echo "Cleaning Prefetch folder..."
+        sudo rm -rf /var/log/apt/*
+    fi
+else
+    echo "Error: CLEAN_PREFETCH is not defined or not an integer."
 fi
 
-# Nettoyer le cache de mise à jour si la case à cocher est cochée
-if [ "$CLEAN_UPDATE_CACHE" -eq 1 ]; then
-    echo "Cleaning Update Cache..."
-    sudo apt-get clean
+if [[ -n "$CLEAN_UPDATE_CACHE" && "$CLEAN_UPDATE_CACHE" =~ ^[0-9]+$ ]]; then
+    # Nettoyer le cache de mise à jour si la case à cocher est cochée
+    if [ "$CLEAN_UPDATE_CACHE" -eq 1 ]; then
+        echo "Cleaning Update Cache..."
+        sudo apt clean
+    fi
+else
+    echo "Error: CLEAN_UPDATE_CACHE is not defined or not an integer."
 fi
 
-# Nettoyer les logs si la case à cocher est cochée
-if [ "$CLEAN_LOGS" -eq 1 ]; then
-    echo "Cleaning Logs..."
-    sudo journalctl --vacuum-time=7d
+if [[ -n "$CLEAN_LOGS" && "$CLEAN_LOGS" =~ ^[0-9]+$ ]]; then
+    # Nettoyer les logs si la case à cocher est cochée
+    if [ "$CLEAN_LOGS" -eq 1 ]; then
+        echo "Cleaning Logs..."
+        sudo journalctl --vacuum-time=7d
+    fi
+else
+    echo "Error: CLEAN_LOGS is not defined or not an integer."
 fi
+
+# Appeler la fonction pour nettoyer le cache du navigateur
+browser_cleaners/browser_clean.sh "$6"
 
 echo "Cleaning completed."
--- cleanuxfront.py
+++ cleanuxfront.py
@@ -1,50 +1,50 @@
 import tkinter as tk
-from tkinter import IntVar, ttk
-import subprocess, sys
-import os
+from tkinter import StringVar, ttk
+import subprocess
 
 # Déclaration des variables globales
 root = None
 clean_temp_var = None
-clean_windows_temp_var = None
+clean_linux_temp_var = None
 clean_prefetch_var = None
 clean_update_cache_var = None
 clean_logs_var = None
 status_label = None
 browser_var = None
+progress_bar = None
 
-def execute_bash_script():
+# Chemins vers les fichiers des scripts bash
+clean_system_script_path = "cleanux.sh"
+clean_browser_cache_script_path = "browser_cleaners/browser_clean.sh"
+
+
+def execute_bash_script(script_path, progress_bar, *args):
     try:
-        # Afficher un message indiquant que le nettoyage est en cours
-        status_label.config(text="Cleaning in progress...")
+        # Démarrer la barre de progression
+        progress_bar.start()
 
-        # Récupérer les valeurs des cases à cocher
-        clean_temp = clean_temp_var.get()
-        clean_windows_temp = clean_windows_temp_var.get()
-        clean_prefetch = clean_prefetch_var.get()
-        clean_update_cache = clean_update_cache_var.get()
-        clean_logs = clean_logs_var.get()
+        # Exécuter le script bash avec les arguments
+        process = subprocess.Popen(["bash", script_path] + list(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        # Récupérer le navigateur sélectionné
-        selected_browser = browser_var.get()
+        # Lire les sorties standard et d'erreur en temps réel pour surveiller l'état du processus
+        for line in process.stdout:
+            # Mettre à jour la barre de progression (vous devrez peut-être ajuster cette logique en fonction du comportement réel de votre script bash)
+            progress_bar.step(1)
+            root.update_idletasks()
 
-        # Chemin absolu vers le script bash unique pour tous les navigateurs
-        cleanux_path = os.path.join(os.path.dirname(__file__), "cleanux.sh")
+        # Attendre que le processus se termine
+        output, error = process.communicate()
 
-        # Chemin absolu vers le script browser_clean.sh spécifique au navigateur
-        browser_script_path = os.path.join(os.path.dirname(__file__), "browser_cleaners", "browser_clean.sh")
+        # Arrêter la barre de progression
+        progress_bar.stop()
 
-        # Exécuter les scripts bash en parallèle
-        process_cleanux = subprocess.run(cleanux_path, shell=False, executable="/bin/bash")
-        process_browser = subprocess.Popen(["bash", browser_script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # Afficher la sortie du script bash dans la console Python
+        print(output.decode())
+        print(error.decode())
 
-        # Attendre que les deux processus se terminent
-        output_cleanux, error_cleanux = process_cleanux.communicate()
-        output_browser, error_browser = process_browser.communicate()
-
-        # Vérifier si une erreur s'est produite dans l'un des processus
-        if process_cleanux.returncode != 0 or process_browser.returncode != 0:
-            raise Exception(f"Error occurred: {error_cleanux.decode()} {error_browser.decode()}")
+        # Vérifier si une erreur s'est produite dans le processus
+        if process.returncode != 0:
+            raise Exception("Error occurred")
 
         # Exemple de message de succès (à remplacer par votre logique réelle)
         status_label.config(text="The cleaning was executed successfully.", fg="green")
@@ -54,28 +54,33 @@ def execute_bash_script():
         status_label.config(text=f"The cleaning failed successfully : {str(e)}", fg="red")
 
     finally:
+        # Arrêter la barre de progression si elle est encore en cours
+        progress_bar.stop()
+
         # Mettre à jour l'étiquette pour indiquer que le nettoyage est terminé
         root.after(3000, clear_status_label)
+
 
 def clear_status_label():
     # Effacer le texte de l'étiquette de statut après un certain délai
     status_label.config(text="", fg="black")
 
+
 def create_settings_frame(parent_frame):
     # Création du cadre de paramètres
-    menu_frame = tk.Frame(parent_frame, bg="#f8f9fa")
+    menu_frame = tk.Frame(parent_frame, bg="#f8f9fa", padx=10, pady=10)
 
-    global clean_temp_var, clean_windows_temp_var, clean_prefetch_var, clean_update_cache_var, clean_logs_var, browser_var
-    clean_temp_var = IntVar(value=1)
-    clean_windows_temp_var = IntVar(value=1)
-    clean_prefetch_var = IntVar(value=1)
-    clean_update_cache_var = IntVar(value=1)
-    clean_logs_var = IntVar(value=1)
+    global clean_temp_var, clean_windows_temp_var, clean_prefetch_var, clean_update_cache_var, clean_logs_var, browser_var, progress_bar
+    clean_temp_var = StringVar(value="1")
+    clean_windows_temp_var = StringVar(value="1")
+    clean_prefetch_var = StringVar(value="1")
+    clean_update_cache_var = StringVar(value="1")
+    clean_logs_var = StringVar(value="1")
 
     clean_temp_checkbox = ttk.Checkbutton(menu_frame, text="Clean Temp folder", variable=clean_temp_var)
     clean_temp_checkbox.grid(row=0, column=0, padx=10, pady=5, sticky="w")
 
-    clean_windows_temp_checkbox = ttk.Checkbutton(menu_frame, text="Clean Windows Temp folder", variable=clean_windows_temp_var)
+    clean_windows_temp_checkbox = ttk.Checkbutton(menu_frame, text="Clean Linux Temp folder", variable=clean_windows_temp_var)
     clean_windows_temp_checkbox.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
     clean_prefetch_checkbox = ttk.Checkbutton(menu_frame, text="Clean Prefetch folder", variable=clean_prefetch_var)
@@ -87,7 +92,7 @@ def create_settings_frame(parent_frame):
     clean_logs_checkbox = ttk.Checkbutton(menu_frame, text="Clean Logs", variable=clean_logs_var)
     clean_logs_checkbox.grid(row=2, column=0, columnspan=2, padx=10, pady=5, sticky="w")
 
-    browser_label = ttk.Label(menu_frame, text="Choose Browser:")
+    browser_label = ttk.Label(menu_frame, text="Choose Browser:", font=("Helvetica", 10))
     browser_label.grid(row=3, column=0, padx=10, pady=5, sticky="w")
 
     browser_options = ["firefox", "brave", "opera", "chromium"]
@@ -95,7 +100,13 @@ def create_settings_frame(parent_frame):
     browser_combobox = ttk.Combobox(menu_frame, textvariable=browser_var, values=browser_options, state="readonly", width=20)
     browser_combobox.grid(row=3, column=1, padx=10, pady=5, sticky="w")
 
+    # Ajout de la barre de progression
+    global progress_bar
+    progress_bar = ttk.Progressbar(menu_frame, orient="horizontal", mode="indeterminate", length=200)
+    progress_bar.grid(row=4, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
+
     return menu_frame
+
 
 def main():
     global root, status_label
@@ -119,8 +130,13 @@ def main():
     settings_frame.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
 
     # Ajout d'un bouton "Clean Linux"
-    clean_button = tk.Button(root, text="Clean Linux", font=("Helvetica", 14), bg="#007BFF", fg="white", padx=20, pady=10, relief="flat", 
-                             command=execute_bash_script)
+    clean_button = tk.Button(root, text="Clean Linux", font=("Helvetica", 14), bg="#007BFF", fg="white", padx=20, pady=10, relief="flat",
+                             command=lambda: execute_bash_script(clean_system_script_path, progress_bar,
+                                                                 clean_temp_var.get(),
+                                                                 clean_windows_temp_var.get(),
+                                                                 clean_prefetch_var.get(),
+                                                                 clean_update_cache_var.get(),
+                                                                 clean_logs_var.get()))
     clean_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
 
     # Ajout d'une étiquette pour afficher l'état du nettoyage
@@ -137,6 +153,7 @@ def main():
     root.grid_rowconfigure(2, weight=1)
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
